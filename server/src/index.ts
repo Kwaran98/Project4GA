94f0bd52ca1defc7f12b5e14691cd04c3d73826e
Kamalesh Waran <kwaranwork98@gmail.com>
2024-06-05 16:43:04 +0800

    The Start of my Project 4

---
+++ server/src/index.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+
+import {userRouter} from './routes/user';
+
+const app = express();
+
+app.use(express.json()) //everytime data comes in an endpoint in the api we would want it to be in json format, it would be way easier for us
+app.use(cors()) //here we are applying the middleware for cors, if we do not have this, we would get an error everytime we make a request to our own API in our React Application
+
+//this codeline here is where all the endpoints related to the user are going to exist
+//however in our userrotuer function itself there are many other endpoints such as register, login etc
+//this endpoints are all located in the user.ts file that is in the routes folder
+app.use("/user", userRouter)
+
+mongoose.connect("mongodb+srv://egator:iJ5MV3mpl84XNJbR@cluster0.4vtjxuo.mongodb.net/ecommerce1?retryWrites=true&w=majority")
+
+app.listen(3001, () => console.log("Server Started"));
---
+++ server/src/models/user.ts
@@ -0,0 +1,19 @@
+import {Schema, model} from 'mongoose';
+
+//Since we are using typescript, we have to create interfaces that can define the types of our objects
+export interface IUser {
+    _id?: string;
+    username: string;
+    password: string;
+    availableMoney: number;
+    // purchasedItems: string[];
+}
+
+const UserSchema = new Schema<IUser>({
+    username: {type: String, require: true, unique: true},
+    password: {type: String, required: true},
+    availableMoney: {type: Number, default: 5000},
+    // purchasedItems
+});
+
+export const UserModel = model<IUser>("user", UserSchema);
